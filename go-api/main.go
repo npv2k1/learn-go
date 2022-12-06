21211c759dfaf45dea2770bb67f81ace2efc5263
Phạm Văn Nguyên <npv2k1@gmail.com>
2022-12-06 23:41:52 +0700

    Learn gin api

---
+++ go-api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	// "fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	// go "errors"
+)
+
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "Hello", Author: "Nguyen", Quantity: 1},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:3000")
+}
